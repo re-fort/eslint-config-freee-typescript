df5a6d7d49aaceccbd5eca05cb7f60aa64e41f2f
tohashi <dots64if@gmail.com>
2020-06-10 15:47:54 +0900

    chore: update @typescript-eslint to v3

--- index.js
+++ index.js
@@ -24,12 +24,43 @@ module.exports = {
         jsxBracketSameLine: false
       }
     ],
-    "no-console": "warn",
+    'no-console': 'warn',
+    camelcase: 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/prefer-interface': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase']
+      },
+
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE']
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase', 'snake_case'],
+        leadingUnderscore: 'allow'
+      },
+      {
+        selector: 'property',
+        format: ['camelCase', 'snake_case']
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require'
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase']
+      }
+    ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn'
   },
